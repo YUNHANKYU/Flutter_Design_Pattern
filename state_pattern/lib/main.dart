039d56b6cfcfbf21f4b1663afc36373de7dabdc0
YUNHANKYU <bhgt4563@gmail.com>
2019-12-12 18:31:06 +0900

    [ADD] State Pattern Example

---
+++ state_pattern/lib/main.dart
@@ -0,0 +1,83 @@
+import 'states.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  NoteBook noteBook;
+  Color textColor;
+
+  @override
+  void initState() {
+    super.initState();
+    noteBook = NoteBook(Off());
+    textColor = Colors.black;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "State Pattern Example",
+      theme: ThemeData(
+        primaryColor: noteBook.bgColor,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("State Pattern Example"),
+        ),
+        body: Container(
+          color: noteBook.bgColor,
+          child: Column(
+            children: <Widget>[
+              Flexible(
+                flex: 3,
+                child: Container(),
+              ),
+              Flexible(
+                  flex: 1,
+                  child: Center(
+                      child: Text(
+                          noteBook.state.toString(),
+                        style: TextStyle(
+                          fontSize: 40.0,
+                          color: textColor,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                  )),
+              Flexible(
+                flex: 6,
+                child: Container(),
+              ),
+              Flexible(
+                flex: 3,
+                child: Center(
+                  child: IconButton(
+                    iconSize: 70.0,
+                    icon: Icon(Icons.fingerprint, color: textColor,),
+                    onPressed: () {
+                      noteBook.powerPush();
+                      setState(() {
+                        textColor = Colors.white;
+                      });
+                    },
+                  ),
+                ),
+              ),
+              Flexible(
+                flex: 5,
+                child: Container(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ state_pattern/lib/states.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+abstract class PowerState {
+  void handler(NoteBook context);
+  String toString();
+}
+
+class Off implements PowerState {
+  handler(NoteBook context) {
+    context.bgColor = Colors.tealAccent;
+    context.state = On();
+  }
+
+  @override String toString() {
+    return "전원 OFF";
+  }
+}
+
+class On implements PowerState {
+  handler(NoteBook context) {
+    context.bgColor = Colors.deepOrange;
+    context.state = Saving();
+  }
+
+  @override String toString() {
+    return "전원 ON";
+  }
+}
+
+class Saving implements PowerState {
+  handler(NoteBook context) {
+    context.bgColor = Colors.black;
+    context.state = Off();
+  }
+
+  @override String toString() {
+    return "절전 모드";
+  }
+}
+
+class NoteBook {
+  PowerState _state;
+  Color bgColor = Colors.white;
+
+  NoteBook(this._state);
+
+  PowerState get state => _state;
+  set state(PowerState newState) => _state = newState;
+
+  void powerPush() {
+    _state.handler(this);
+  }
+}
